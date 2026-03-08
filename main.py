3c7e01d044d368b246c55230422e7dca3a2049a5
Emmanuel <mono64212@gmail.com>
2026-03-07 19:20:35 -0500

    Implementacion de Endpoint, post y get

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
-from models import Reserva 
+from models import Reserva
+from typing import List 
 
 app = FastAPI()
 
@@ -8,3 +9,12 @@ db_reservas = []
 @app.get("/")
 def inicio():
     return {"mensaje": "Bienvenido al sistema de reservas del ITM"}
+
+@app.post("/reservas")
+def registrar_reserva(nueva_reserva: Reserva):
+    db_reservas.append(nueva_reserva)
+    return {"mensaje": "Reserva registrada con éxito", "data": nueva_reserva}
+
+@app.get("/reservas", response_model=List[Reserva])
+def consultar_reservas():
+    return db_reservas
